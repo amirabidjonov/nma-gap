f1de52083ce22187c9b307ab2171ea9deec94b82
DALERdeveloperWEP <cassidymlbbbbb@gmail.com>
2026-06-10 17:39:30 +0500

    fixed bug

--- message.py
+++ message.py
@@ -3,5 +3,4 @@ message = {
     "first_phone_number": "Iltimos telefon raqamingizni yuboring",
     "save_contact": "Telefon raqamingiz saqlandi",
     "orders": "Sizda hali birorta ham buyurtma yo`q",
-    
 }
